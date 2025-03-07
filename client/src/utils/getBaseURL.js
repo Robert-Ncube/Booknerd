4ff8f564f76e54c1a36e91c6e098457c7b462272
Robert Ncube <robertncube6940@gmail.com>
2025-03-07 12:49:57 +0200

    changed the client baseURL for production.

--- client/src/utils/getBaseURL.js
+++ client/src/utils/getBaseURL.js
@@ -1,3 +1,12 @@
 export const getBaseURL = () => {
-  return "http://localhost:5000";
+  return "https://booknerd-backend.vercel.app";
 };
+
+/*
+ * In a production environment, replace 'http://localhost:5000' with https://booknerd-backend.vercel.app, vise versa.
+ * This function can be used in multiple places throughout the application.
+ * For example, when making API requests, you can use the following syntax:
+ * axios.get(`${getBaseURL()}/api/books`)
+ *
+ * Make sure to replace '/api/books' with the appropriate endpoint for your API.
+ */
